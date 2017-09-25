7f3e13572c244000c984075646e2d161e15735ba
Alexander Letnikow <reclamme@gmail.com>
2017-09-25 14:58:53 +0300

    Add Request class

---
+++ src/Request.php
@@ -0,0 +1,18 @@
+<?php
+
+
+namespace Wearesho\Yii\Http;
+
+use yii\web\JsonParser;
+use yii\web\Request as WebRequest;
+
+/**
+ * Class Request
+ * @package Wearesho\Yii\Http
+ */
+class Request extends WebRequest
+{
+    public $parsers = [
+        'application/json' => JsonParser::class,
+    ];
+}
