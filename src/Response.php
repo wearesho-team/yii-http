b0679dea7c7c8e6669471afcc8711c573d3ad54f
Alexander Letnikow <reclamme@gmail.com>
2017-09-25 14:33:52 +0300

    Add Response class (with validation exception code settings)

---
+++ src/Response.php
@@ -0,0 +1,30 @@
+<?php
+
+
+namespace Wearesho\Yii\Http;
+
+use Wearesho\Yii\Http\Exceptions\ValidationException;
+use yii\web\Response as WebResponse;
+
+/**
+ * Class Response
+ * @package Wearesho\Yii\Http
+ */
+class Response extends WebResponse
+{
+    public $format = self::FORMAT_JSON;
+
+    /**
+     * @param \Error|\Exception $e
+     * @return $this|\yii\web\Response
+     */
+    public function setStatusCodeByException($e)
+    {
+        if ($e instanceof ValidationException) {
+            $this->setStatusCode(400);
+            return $this;
+        }
+
+        return parent::setStatusCodeByException($e);
+    }
+}
