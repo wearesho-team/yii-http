9d282bce944c17086ef4e842bac098a8653fdb61
Alexander Letnikow <reclamme@gmail.com>
2017-12-25 12:14:41 +0200

    Refactor Form, reformat panel and form constructors

--- src/Form.php
+++ src/Form.php
@@ -23,22 +23,16 @@ abstract class Form extends Panel
      * @param Connection $connection
      * @param array $config
      */
-    public function __construct(Request $request, Response $response, Connection $connection, array $config = [])
+    public function __construct(
+        Request $request,
+        Response $response,
+        Connection $connection,
+        array $config = []
+    )
     {
         parent::__construct($request, $response, $config);
 
         $this->connection = $connection;
-    }
-
-    /**
-     * @return void
-     */
-    public function init()
-    {
-        parent::init();
-
-        $this->on(BaseController::EVENT_BEFORE_ACTION, [$this, 'begin']);
-        $this->on(BaseController::EVENT_AFTER_ACTION, [$this, 'commit']);
     }
 
     /**
@@ -47,50 +41,8 @@ abstract class Form extends Panel
      */
     public function getResponse(): Response
     {
-        try {
+        return $this->connection->transaction(function() {
             return parent::getResponse();
-        } catch (\Throwable $exception) {
-            $this->rollBack();
-            throw $exception;
-        }
-    }
-
-    /**
-     * @return static
-     */
-    protected function begin()
-    {
-        $transaction = $this->connection->getTransaction();
-        if (!$transaction || !$transaction->isActive) {
-            $this->connection->beginTransaction();
-        }
-
-        return $this;
-    }
-
-    /**
-     * @return static
-     */
-    protected function commit()
-    {
-        $transaction = $this->connection->getTransaction();
-        if ($transaction && $transaction->isActive) {
-            $transaction->commit();
-        }
-
-        return $this;
-    }
-
-    /**
-     * @return $this
-     */
-    protected function rollBack()
-    {
-        $transaction = $this->connection->getTransaction();
-        if ($transaction && $transaction->isActive) {
-            $transaction->rollBack();
-        }
-
-        return $this;
+        });
     }
 }
