c4a5dc68a9fa1f334c3fed7f8f16e771aa252c00
KartaviK <varkuta.roma@gmail.com>
2018-08-07 12:29:50 +0300

    fix test response format trigger

--- tests/Behaviors/ResponseFormatTest.php
+++ tests/Behaviors/ResponseFormatTest.php
@@ -12,17 +12,6 @@ use yii\base;
  */
 class ResponseFormatTest extends Http\Tests\AbstractTestCase
 {
-    public function testSet(): void
-    {
-        $response = new Http\Response();
-        $behavior = new Http\Behaviors\ResponseFormat($response, [
-            'format' => Http\Response::FORMAT_RAW,
-        ]);
-        $behavior->setFormat();
-
-        $this->assertEquals(Http\Response::FORMAT_RAW, $response->format);
-    }
-
     /**
      * @expectedException \yii\base\InvalidConfigException
      * @expectedExceptionMessage Invalid response format
@@ -35,38 +24,31 @@ class ResponseFormatTest extends Http\Tests\AbstractTestCase
 
     public function testTrigger(): void
     {
-        $fakeController = new class('id_controller', new base\Module('id_module')) extends Http\Controller
+        \Yii::$app->response->format = Http\Response::FORMAT_HTML;
+
+        $fakeController = new class('id', new base\Module('id')) extends Http\Controller
         {
-            public function __construct(string $id, base\Module $module, array $config = [])
-            {
-                parent::__construct($id, $module, $config);
-            }
+            public $actions = [
+                'test' => ['get' => 'test',],
+            ];
 
             public function behaviors(): array
             {
                 return [
-                    'get' => [
+                    'responseFormat' => [
                         'class' => Http\Behaviors\ResponseFormat::class,
                         'format' => Http\Response::FORMAT_JSON,
                     ],
                 ];
             }
-
-            public function actionTest(): array
-            {
-                return [
-                    'key' => 'value'
-                ];
-            }
         };
 
         $fakeController->enableCsrfValidation = false;
-        $action = $fakeController->createAction('test');
-        $fakeController->trigger(Http\Controller::EVENT_BEFORE_ACTION);
-
-        $this->assertArraySubset(
-            ['key' => 'value',],
-            $action->runWithParams([])
+        $fakeController->trigger(
+            Http\Controller::EVENT_BEFORE_ACTION,
+            new base\ActionEvent($fakeController->createAction('test'))
         );
+
+        $this->assertEquals(Http\Response::FORMAT_JSON, \Yii::$app->response->format);
     }
 }
