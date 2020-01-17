62675f67b7b9294772acd8c1e04caa8cc4a2e2b5
Alexander Letnikow <reclamme@gmail.com>
2020-01-17 05:46:49 +0200

    Edit cors settings

--- src/Controller.php
+++ src/Controller.php
@@ -55,6 +55,12 @@ class Controller extends web\Controller
             ],
             'corsFilter' => [
                 'class' => filters\Cors::class,
+                'cors' => [
+                    'Origin' => ['*'],
+                    'Access-Control-Request-Method' => ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
+                    'Access-Control-Request-Headers' => ['*'],
+                    'Access-Control-Max-Age' => 86400,
+                ],
             ],
             'verbs' => [
                 'class' => filters\VerbFilter::class,
